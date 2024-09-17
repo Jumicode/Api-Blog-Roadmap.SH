6dc2bd418fbe1de74b7e365da3feab9c36a967a9
Julio Romero <jromeroacive@gmail.com>
2024-09-17 11:45:42 -0400

    migrations

---
+++ app/Http/Controllers/ApiblogController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Apiblog;
+use Illuminate\Http\Request;
+
+class ApiblogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Apiblog $apiblog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Apiblog $apiblog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Apiblog $apiblog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Apiblog $apiblog)
+    {
+        //
+    }
+}
