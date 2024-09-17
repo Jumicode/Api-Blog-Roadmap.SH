b66f57d1946077d6c4dad5538d4ebdc2817551b8
Julio Romero <jromeroacive@gmail.com>
2024-09-17 16:36:26 -0400

    Route Api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiblogController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::get('/blog', [ApiblogController::class, 'index']);
+
+Route::get('/blog/{id}', [ApiblogController::class,'show']);
+
+Route::post('/blog', [ApiblogController::class, 'store']);
+
+Route::put('/blog/{id}', [ApiblogController::class,'update']);
+
+Route::patch('/blog/{id}', [ApiblogController::class,'update']);
+
+Route::delete('/blog/{id}', [ApiblogController::class, 'destroy']);
+
