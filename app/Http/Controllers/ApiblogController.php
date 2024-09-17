f9ec5478de004e4cd38ae35bb0c422963dc185ac
Julio Romero <jromeroacive@gmail.com>
2024-09-17 16:10:33 -0400

    Index, store, destroy, update and show functions

--- app/Http/Controllers/ApiblogController.php
+++ app/Http/Controllers/ApiblogController.php
@@ -1,65 +1,172 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Apiblog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ApiblogController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
-        //
+     $blogs = Apiblog::All();
+
+
+     $data = [
+        'blogs' => $blogs,
+        'status' => 200
+     ];
+
+
+     return response()->json($data, 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:25',
+            'content' => 'required|max:500',
+            'category' => 'required|max:15',
+            'tags' => 'required|max:100'
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                'message' => 'Error ',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+        $blogs = Apiblog::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'category' => $request->caterogy,
+            'tags' => $request->tags
+        ]);
+
+        if (!$student) {
+            $data = [
+                'message' => 'Error ',
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+      
+     $data = [
+        'blogs' => $blogs,
+        'status' => 201
+     ];
+
+
+        return response()->json($data, 201);
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Apiblog $apiblog)
+    public function show($id)
     {
-        //
+        $blogs = Apiblog::find($id);
+
+        if (!$blogs) {
+            $data = [
+                'message' => 'Error',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $data = [
+            'student' => $blogs,
+            'status' => 200
+        ];
+
+        return response()->json($data, 200); 
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Apiblog $apiblog)
-    {
-        //
-    }
+
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Apiblog $apiblog)
+    public function update(Request $request, $id)
     {
-        //
+        $blogs = Apiblog::find($id);
+
+        if (!$blogs) {
+            $data = [
+                'message' => 'Error',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:25',
+            'content' => 'required|max:500',
+            'category' => 'required|max:15',
+            'tags' => 'required|max:100'
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                'message' => 'Error en la validación de los datos',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+
+        $blogs->title = $request->title;
+        $blogs->content = $request->content;
+        $blogs->category = $request->category;
+        $blogs->tags = $request->tags;
+
+        $blogs->save();
+
+        $data = [
+            'message' => 'Success',
+            '$blogs' => $blogs,
+            'status' => 200
+        ];
+
+        return response()->json($data, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Apiblog $apiblog)
+    public function destroy($id)
     {
-        //
+        $blogs = Apiblog::find($id);
+
+        if (!$blogs) {
+            $data = [
+                'message' => 'Error',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $student->delete();
+
+        $data = [
+            'message' => 'Delete',
+            'status' => 204
+        ];
+
+        return response()->json($data, 204);
     }
 }
